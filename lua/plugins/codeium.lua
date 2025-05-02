50202128adafdc5012954fcb04dccaae25b9f530
TheInfernitex <agarwalparthsplendid@gmail.com>
2025-05-02 21:09:27 +0530

    fix: codeium suggestions

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,7 +1,30 @@
 return {
-  "Exafunction/codeium.nvim",
-  event = "BufEnter",
-  config = function()
-    require("codeium").setup({})
-  end,
+    "Exafunction/codeium.nvim",
+    event = "InsertEnter",
+    build = ":Codeium Auth",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
+            enable_tab_fallback = true,
+        })
+        -- Optional keybindings
+        vim.keymap.set("i", "<C-g>", function()
+            return vim.fn["codeium#Accept"]()
+        end, { expr = true, silent = true })
+
+        vim.keymap.set("i", "<C-;>", function()
+            return vim.fn
+        end, { expr = true, silent = true })
+
+        vim.keymap.set("i", "<C-,>", function()
+            return vim.fn["codeium#CycleCompletions"](-1)
+        end, { expr = true, silent = true })
+
+        vim.keymap.set("i", "<C-x>", function()
+            return vim.fn["codeium#Clear"]()
+        end, { expr = true, silent = true })
+    end,
 }
