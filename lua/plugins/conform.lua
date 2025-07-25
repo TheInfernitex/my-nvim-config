2b4c5dd0bce69ae37e5c1a367186784266a3b8f4
TheInfernitex <agarwalparthsplendid@gmail.com>
2025-07-25 16:01:18 +0530

    remove neotree, edit conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,19 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            javascript = { "prettier" },
-            typescript = { "prettier" },
-            java = { "prettier" },
-            javascriptreact = { "prettier" },
-            typescriptreact = { "prettier" },
-            json = { "prettier" },
-            yaml = { "prettier" },
-            markdown = { "prettier" },
-            html = { "prettier" },
-            css = { "prettier" },
-            scss = { "prettier" },
-        },
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      java = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      rust = { "rustfmt" },
     },
+  },
 }
